f7278abdee0ff95f44e25744c00201fa423e25b1
Josefine Forsman <josefineforsman1@gmail.com>
2023-05-26 15:27:22 +0200

    cypress files created

--- strajk/cypress/e2e/strajk.cy.js
+++ strajk/cypress/e2e/strajk.cy.js
@@ -1,5 +1,12 @@
-describe('template spec', () => {
+describe('Strajk spec', () => {
   it('passes', () => {
-    cy.visit('http://localhost:5174/')
+    cy.visit('http://localhost:5174/#')
   })
+  // it('get api nr', () =>{
+  //   const date = '2023-06-12';
+
+  //   cy.get('.input__field.booking-info__date').should('have.text', date);
+
+   
+  // })
 })
